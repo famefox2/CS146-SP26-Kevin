bf37d2dd2a5d1fa16b0b547cb831228b3f885ea1
famefox2 <54617980+famefox2@users.noreply.github.com>
2026-02-09 22:43:53 -0800

    Update Partition.java

--- CS146HW2/Partition/src/Partition.java
+++ CS146HW2/Partition/src/Partition.java
@@ -12,24 +12,24 @@ public class Partition {
      * @return the partition index
      */
     public static int hoarePartition(int[] array, int low, int high){
-        int key = array[0];
-        int i = low-1;
-        int j = high+1;
-        int temp;
+        int key = array[0]; // The numbers in the array to compare to
+        int i = low-1; //Starting index at the begining of array
+        int j = high+1;// Starting index at the end of the array
+        int temp;// Keep track of a number in array before the swapped with another number in array
 
         while(i<=j){
-            do{ 
+            do{ // deincrement j while the number in the array is greater than key value
                 j-=1;
             } while (array[j]>key);
 
-            do{
+            do{//increment i while the number in the array is less than key value
                 i+=1;
             } while(array[i]<key);
 
-            if(i>=j){
+            if(i>=j){//After i pass j return j value index
                 return j;
             }
-            temp = array[j];
+            temp = array[j]; // Swap i and j value
             array[j]= array[i];
             array[i] = temp;
             
@@ -44,12 +44,12 @@ public class Partition {
      * @return the partition index
      */
     public static int lomutoPartition(int[] array,int low,int high){  
-        int pivot = array[high];
-        int temp;
-        int i = low-1;
+        int pivot = array[high];// The number that is compared to in the array.
+        int temp; // Keep track of a number in array before the swapped with another number in array
+        int i = low-1; //Starting index of the method
 
-        for(int j = low; j < high;++j){
-            if(array[j]<= pivot){
+        for(int j = low; j < high;++j){//loop through the array
+            if(array[j]<= pivot){// Swap numbers when number in the array is less than or equal pivot
                 i+=1;
                 temp = array[j];
                 array[j] = array[i];
@@ -69,16 +69,16 @@ public class Partition {
      * @param index partition index of the array
      */
     public static void printAll(int[] a, String name, int index){
-        System.out.println(name);
-        for(int i: a){
+        System.out.println(name);//Print method used
+        for(int i: a){//Print the array
             System.out.print(i + ", ");
         }
         System.out.println();
         
-        if(name.equals( "Hoare")){
+        if(name.equals( "Hoare")){//Print Hoare partitions
             printPartitionsH(a, index+1);
         }
-        else{
+        else{//Print lomuto partitions
             
             printPartitionL(a, index);
         }
@@ -87,35 +87,35 @@ public class Partition {
 
    
     /**
-     * Print the partitions of the array
+     * Print the partitions of the array for Hoare Method
      * @param a the enitre array
      * @param index the index partition to split the array in two
      */
     public static void printPartitionsH(int[] a, int index){
-        System.out.print("Left{");
+        System.out.print("Left{");//print left side of partition
         for(int i =0; i< index; ++i){
             System.out.print(a[i] + ", ");
         }
          System.out.print("} Right{");
 
-        for(int j= index; j < a.length; ++j){
+        for(int j= index; j < a.length; ++j){//print right side of partition
             System.out.print(a[j] + ", ");
         }
         System.out.println("}");
     }
     /**
-     * Print the partitions for lomuto method
+     * Print the partitions for lomuto method without the pivot number/value that is in the right index position
      * @param a the enitre array
      * @param index the index partition to split the array in two
      */
     public static void printPartitionL(int[] a, int index){
         System.out.print("Left{");
-        for(int i =0; i< index; ++i){
+        for(int i =0; i< index; ++i){//print left side of partition
             System.out.print(a[i] + ", ");
         }
          System.out.print("} Right{");
 
-        for(int j= index+1; j < a.length; ++j){
+        for(int j= index+1; j < a.length; ++j){//print right side of partition
             System.out.print(a[j] + ", ");
         }
         System.out.println("}");
@@ -127,9 +127,9 @@ public class Partition {
         //Unit test 1
         int[] testArray1 = {4,6,9,3,7};
         int[] testArray2 = {4,6,9,3,7};
-        int h = hoarePartition(testArray1,0,4);
+        int h = hoarePartition(testArray1,0,4); //index of partition for hoare
 
-        int l = lomutoPartition(testArray2, 0, 4);
+        int l = lomutoPartition(testArray2, 0, 4);//index of partition for lomuto
         System.out.println("Test 1-Before: {4,6,9,3,7}");
         printAll(testArray1, hoare, h);
         printAll(testArray2, lomoto, l);
@@ -137,8 +137,8 @@ public class Partition {
         // Test 2
         int[] testArray5 = {84,3,7,1,9,6,2,5,};
         int[] testArray6 = {84,3,7,1,9,6,2,5,};
-        int h3 = hoarePartition(testArray5,0, testArray5.length-1);
-        int l3 = lomutoPartition(testArray6, 0, testArray6.length-1);
+        int h3 = hoarePartition(testArray5,0, testArray5.length-1); //index of partition for hoare
+        int l3 = lomutoPartition(testArray6, 0, testArray6.length-1);//index of partition for lomuto
 
         System.out.println("Test 2-Before: {84,3,7,1,9,6,2,5,}");
         printAll(testArray5, hoare, h3);
@@ -147,26 +147,26 @@ public class Partition {
         //Test 3
         int[] testArray7 = {10,17,19,21,44,55,57,63,65,67};
         int[] testArray8 = {10,17,19,21,44,55,57,63,65,67};
-        int h4 = hoarePartition(testArray7,0,testArray7.length-1);
-        int l4 = lomutoPartition(testArray8, 0,testArray8.length-1);
+        int h4 = hoarePartition(testArray7,0,testArray7.length-1);//index of partition for hoare
+        int l4 = lomutoPartition(testArray8, 0,testArray8.length-1);//index of partition for lomuto
 
         System.out.println("Test 3-Before: {10,17,19,21,44,55,57,63,65,67}");
         printAll(testArray7, hoare, h4);
         printAll(testArray8, lomoto, l4);
        //Test 4
-       try{ 
+       try{ //Test the empty set
         System.out.println("Test 4-Before: {}");
         int[] testArray3 = {};
         int[] testArray4 = {};
-        int h2 = hoarePartition(testArray3, 0,0);
+        int h2 = hoarePartition(testArray3, 0,0);//index of partition for hoare
 
-        int l2 = lomutoPartition(testArray4, 0, 0);
+        int l2 = lomutoPartition(testArray4, 0, 0);//index of partition for lomuto
 
        
         printAll(testArray3, hoare, h2);
         printAll(testArray4, lomoto, l2);
        }
-       catch(Exception e){
+       catch(Exception e){//Methods fail due to the array being empty
             System.out.println("Empty Array");
        }
        /*
@@ -178,6 +178,7 @@ public class Partition {
        
     }
 }
+
 
 
 
