d0be975fd0528fdc3b03dbf6ad40fd81830059c2
famefox2 <54617980+famefox2@users.noreply.github.com>
2026-02-14 01:45:08 -0800

    Update WordFreq.java
    Added build max heap to go through the array list to build heap

--- CS146-W3HW_Kevin/CS146W3HW/src/WordFreq.java
+++ CS146-W3HW_Kevin/CS146W3HW/src/WordFreq.java
@@ -1,7 +1,6 @@
 import java.util.ArrayList;
-
 /**
- * WordFreq stores a phrase and its frequency. 
+ * WordFreq stores a phrase and its frequency. Builds a max heap of an arraylist of WordFreq.
  * @author Kevin Thai 
  */
 public class WordFreq {
@@ -24,7 +23,7 @@ public class WordFreq {
         return word;
     }
     /**
-     * Set the phrase to 
+     * Set the new phrase to word
      * @param word The phrase
      */
     public void setWord(String word) {
@@ -45,29 +44,46 @@ public class WordFreq {
         this.frequency = frequency;
     }
 
+    /**
+     * Compare frequency of parent node to left and right child node 
+     * and swap parent node with child if the child node have the largest frequency. 
+     * If there is a swap run max heap again on the that child node
+     * @param list list of WordFreq onjects
+     * @param i the parent node index
+     */
     public static void maxHeap(ArrayList<WordFreq> list, int i){
-        int largest;
+        WordFreq temp;// Placeholder for swap
+        int largest;// Index of largest frequency node
         int leftNode = 2*i+1; // left child node index
         int rightNode = 2*i+2; // right child node index
-        if(leftNode<= list.size() && list.get(leftNode).getFrequency() > list.get(i).getFrequency()){
+        if(leftNode< list.size() && list.get(leftNode).getFrequency() > list.get(i).getFrequency()){
             largest = leftNode;
         }
         else{
             largest = i;
         }
-        if(rightNode <= list.size() && list.get(rightNode).getFrequency() > list.get(largest).getFrequency()){
+
+        if(rightNode < list.size() && list.get(rightNode).getFrequency() > list.get(largest).getFrequency()){
             largest = rightNode;
         }
-        System.out.println(list.get(largest).toString());
 
+        if(largest != i){// Swap parent node with child node if child is larger
+            temp = list.get(i);
+            list.set(i, list.get(largest));
+            list.set(largest, temp);
+            maxHeap(list, largest);// Run max heap on child node
+        }
+        
     }
     /**
      * Builds a max heap from array list of wordfreq objects
      * @param list Array List of wordfreq objects
      */
     public static void buildMaxHeap(ArrayList<WordFreq> list){
-
-
+        int sIndex = (list.size()+1)/2-1; //Starting at index before first leaf node
+        for(int i = sIndex; i >= 0;--i ){//Run max heap from non leaf node to root node
+            maxHeap(list, i);
+        }
     }
     /**
      * Return a string of the word and the frequency;
@@ -78,7 +94,8 @@ public class WordFreq {
         return word +", " +frequency;
     }
     public static void main(String[] args) {
-        ArrayList<WordFreq> list = new ArrayList<>();
+
+        ArrayList<WordFreq> list = new ArrayList<>();//ArrayList of WordFreq objects
         WordFreq phrase = new WordFreq("happy", 400);
         list.add(phrase);
         list.add(new WordFreq("satisfied",100));
@@ -94,15 +111,13 @@ public class WordFreq {
         for(WordFreq wf: list){
             System.out.println(wf.toString());
         }
-        //test max heap method
-        maxHeap(list, 0);
-        System.out.println("Largest should be \"happy, 400\" at index 0");
-        maxHeap(list, 1);
-        System.out.println("Largest should be \"would buy again, 200\" at index 1");
-        maxHeap(list, 3);
-        System.out.println("Largest should be \"would recommend to friends, 800\" at index 3");
-        maxHeap(list, 6);
-        System.out.println("Largest should be \"difficult to use, 100\" at index 6");
+       
+        buildMaxHeap(list);
+        System.out.println();
+        System.out.println("After MaxHeap");
+         for(WordFreq wf: list){
+            System.out.println(wf.toString());
+        }
     }
 
 }
